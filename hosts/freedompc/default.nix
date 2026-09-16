2feefbc4d6e7ba30c20d244e1bbab51803cafac9
AndrianoTurner <danya.shibaev@gmail.com>
2026-09-16 12:02:18 +0300

    fix: pin drivers

--- hosts/freedompc/default.nix
+++ hosts/freedompc/default.nix
@@ -1,4 +1,4 @@
-{config, ...}: {
+{ config, ... }: {
   imports = [
     ./hardware-configuration.nix
     ../../modules/nixos/common
@@ -22,7 +22,14 @@
     powerManagement.finegrained = false;
     open = false; # use proprietary kernel module
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "595.99.02";
+      sha256_64bit = "sha256-6HR3lYv3YwcFSTJL1a1slI66btIQ5EAFs+/4SUD24ew=";
+      sha256_aarch64 = "sha256-CCqHZTN2KNOZ4yZp2rDcuRJp9pHfRw47k4m4dWnS/2w=";
+      openSha256 = "sha256-T36x/jx8yQ8l3LFp1rZIrTfcSwbGy8YSAvXOUSptpb4=";
+      settingsSha256 = "sha256-GYCcnxfKPrTCrsmd25sMyzfC5cqJQJx0c31haooyTYM=";
+      persistencedSha256 = "sha256-VyKtF/HdHPQrHHK6opSO69M72LmnGZtauuchj9uuje8=";
+    };
 
     prime = {
       offload.enable = true;
