c14094fa61d3a387fab44b6b2c341b12069a73a0
AndrianoTurner <danya.shibaev@gmail.com>
2026-09-23 22:57:21 +0300

    feat: hw experimental features

--- modules/nixos/physical.nix
+++ modules/nixos/physical.nix
@@ -6,6 +6,11 @@
     openssh.enable = true;
     power-profiles-daemon.enable = true;
     upower.enable = true;
+    # Scheduler
+    scx = {
+      enable = true;
+      scheduler = "scx_lavd";
+    };
   };
 
   swapDevices = [
@@ -18,7 +23,16 @@
   boot = {
     kernelPackages = pkgs.linuxKernel.packages.linux_xanmod_latest;
     kernelParams = ["usbcore.autosuspend=-1"];
+    # Compressed RAM cache.
+    zswap = {
+      enable = true;
+      compressor = "zstd";
+      maxPoolPercent = 20;
+      shrinkerEnabled = true;
+    };
   };
+
+  zramSwap.enable = false;
 
   powerManagement.powertop.enable = true;
 }
